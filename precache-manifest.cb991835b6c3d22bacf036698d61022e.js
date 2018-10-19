056ab968f3b4b67c7faffe5505dbc34d55aa6663
ericma000 <ericma000@gmail.com>
2018-10-18 17:18:58 -0700

    Updates

---
+++ precache-manifest.cb991835b6c3d22bacf036698d61022e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e2f6c64c276b8cffd5f444d122b89ca7",
+    "url": "/pomodoro/static/media/timer_alarm.e2f6c64c.mp3"
+  },
+  {
+    "revision": "48c98778d1c80a56daf70b031b6d81bb",
+    "url": "/pomodoro/static/media/barking.48c98778.mp3"
+  },
+  {
+    "revision": "a702e67ff471c6ecfc33",
+    "url": "/pomodoro/static/js/runtime~main.a702e67f.js"
+  },
+  {
+    "revision": "9aa73e121e7bb8be8236",
+    "url": "/pomodoro/static/js/main.9aa73e12.chunk.js"
+  },
+  {
+    "revision": "2860e4647c51f62db3e5",
+    "url": "/pomodoro/static/js/1.2860e464.chunk.js"
+  },
+  {
+    "revision": "9aa73e121e7bb8be8236",
+    "url": "/pomodoro/static/css/main.e5e14ff8.chunk.css"
+  },
+  {
+    "revision": "d55abc79d60846b067a05d9dfa9597f2",
+    "url": "/pomodoro/index.html"
+  }
+];
